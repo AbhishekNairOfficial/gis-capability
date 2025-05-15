dd2fc018b90e2b028cc5dd8c2f2f1c9092dc58a0
AbhishekNairOfficial <abhisheknr4@gmail.com>
2025-05-15 16:24:05 -0700

    Fixed color of counties

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -194,7 +194,14 @@ export default function MapComponent({}: MapProps) {
       extruded: false,
       lineWidthMinPixels: 1,
       getLineColor: [255, 255, 255],
-      getFillColor: [128, 0, 128, 100],
+      // getFillColor: [128, 0, 128, 100],
+      getFillColor: (feature: any) => {
+        const totalVoters = feature.properties.SUM_VOTERS;
+        const minVoters = 100;
+        const maxVoters = 8000;
+        const normalized = Math.min(1, Math.max(0, (totalVoters - minVoters) / (maxVoters - minVoters)));
+        return [128, 0, 128, normalized * 1000];
+      },
       visible: layers.countyPoints
     }),
     // Hexagon layer for voting data
